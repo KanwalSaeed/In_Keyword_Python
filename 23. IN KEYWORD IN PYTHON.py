9f21611e5ddad7702cf527c43fb783c96551fd30
KanwalSaeed <kanwal_saeed122@hotmail.com>
2020-02-19 17:12:42 +0500

    first code

---
+++ 23. IN KEYWORD IN PYTHON.py
@@ -0,0 +1,24 @@
+password = "helpme"
+if 'helpme' in password:
+    print("It exit in Password.")
+else:
+    print("It's not exit")
+
+password = "helpme"
+if 'me' in password:
+    print("It exit in Password.")
+else:
+    print("It's not exit")
+password = "helpme"
+if 'm' in password:
+    print("It exit in Password.")
+else:
+    print("It's not exit")
+
+password = "helpme"
+if 'K' in password:
+    print("It exit in Password.")
+else:
+    print("It's not exit")
+
+
